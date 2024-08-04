e53700ceb1bb51a1c54098482b898ce0c1dde624
Jiggly Balls <jigglyballs9000@gmail.com>
2024-08-04 22:43:22 +0400

    Assets

---
+++ graphics/data/Tilesets/Objects.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="Objects" tilewidth="96" tileheight="124" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="60" source="../../images/objects/bush.png"/>
+ </tile>
+ <tile id="1">
+  <image width="56" height="68" source="../../images/objects/merchant.png"/>
+ </tile>
+ <tile id="2">
+  <image width="40" height="40" source="../../images/objects/stump_medium.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="40" source="../../images/objects/stump_small.png"/>
+ </tile>
+ <tile id="4">
+  <image width="56" height="112" source="../../images/objects/sunflower.png"/>
+ </tile>
+ <tile id="5">
+  <image width="96" height="124" source="../../images/objects/tree_medium.png"/>
+ </tile>
+ <tile id="6">
+  <image width="56" height="116" source="../../images/objects/tree_small.png"/>
+ </tile>
+ <tile id="7">
+  <image width="44" height="48" source="../../images/objects/flower.png"/>
+ </tile>
+ <tile id="8">
+  <image width="40" height="44" source="../../images/objects/mushroom.png"/>
+ </tile>
+ <tile id="9">
+  <image width="52" height="52" source="../../images/objects/mushrooms.png"/>
+ </tile>
+</tileset>
