659868d94611e01feacf911472329e2c9e3a3233
Jiggly Balls <jigglyballs9000@gmail.com>
2024-08-08 14:21:06 +0400

    git commit added coin asset & bg surface

--- entities/overlay.py
+++ entities/overlay.py
@@ -1,8 +1,7 @@
 import pygame
 
-from core.settings import Display
-from core.utils import Text
-from core.presets import overlay_style
+from core.settings import Display, COIN_ANIMATIONS, BG_COLOUR
+from core.utils import Animation, import_folder, get_path
 from entities.player import Player
 
 
@@ -10,14 +9,15 @@ class Overlay:
     def __init__(self, player: Player, display: pygame.Surface) -> None:
         self.display_surface = display
         self.player = player
-        self.text = Text(
-            self.display_surface,
-            overlay_style,
+        self.font = pygame.font.Font(
+            get_path(r"..\graphics\LycheeSoda.ttf"),
             30,
-            (
-                Display.SCREEN_RESOLUTION[0] / 1.2,
-                Display.SCREEN_RESOLUTION[1] / 1.17,
-            ),
+        )
+
+        self.coin_animation = Animation(
+            {"coin": [image for image in import_folder(COIN_ANIMATIONS)]},
+            start_status="coin",
+            speed=10,
         )
 
         overlay_path = "graphics/images/overlay"
@@ -30,7 +30,14 @@ class Overlay:
             f"{fruit_path}/apple.png"
         ).convert_alpha()
 
-    def draw(self) -> None:
+        self.all_tool_rect = self.tools_surf[self.player.inventory.selected].get_rect(
+            center=(
+                Display.SCREEN_RESOLUTION[0] / 1.2,
+                Display.SCREEN_RESOLUTION[1] / 1.3,
+            )
+        )
+
+    def draw(self, dt: int) -> None:
         tool_surf = self.tools_surf[self.player.inventory.selected]
         tool_rect = tool_surf.get_rect(
             center=(
@@ -38,7 +45,60 @@ class Overlay:
                 Display.SCREEN_RESOLUTION[1] / 1.3,
             )
         )
-        self.text.render(
-            f"x{self.player.inventory.inv[self.player.inventory.selected]}"
+
+        coin_surf = self.coin_animation.play_status(dt=dt)
+        coin_rect = coin_surf.get_rect(
+            center=(
+                Display.SCREEN_RESOLUTION[0] / 1.25,
+                Display.SCREEN_RESOLUTION[1] / 5,
+            )
         )
+
+        inv_amount_surf = self.font.render(
+            f"x{self.player.inventory.inv[self.player.inventory.selected]}",
+            False,
+            "white",
+        )
+        inv_amount_rect = inv_amount_surf.get_rect(
+            center=(
+                Display.SCREEN_RESOLUTION[0] / 1.2,
+                Display.SCREEN_RESOLUTION[1] / 1.17,
+            )
+        )
+
+        money_surf = self.font.render(f"{self.player.money}", False, "white")
+        money_rect = money_surf.get_rect(
+            center=(
+                Display.SCREEN_RESOLUTION[0] / 1.2,
+                Display.SCREEN_RESOLUTION[1] / 5,
+            )
+        )
+
+        money_bg_surf = pygame.Surface(coin_rect.topleft)
+        money_bg_surf.fill(BG_COLOUR)
+        money_bg_rect = money_bg_surf.get_rect(topleft=coin_rect.topleft).inflate(
+            20, 15
+        )
+        self.display_surface.blit(
+            money_bg_surf,
+            money_bg_rect,
+            money_rect.inflate(350, 90),
+            special_flags=pygame.BLEND_RGBA_MULT,
+        )
+
+        tool_bg_surf = pygame.Surface(self.all_tool_rect.topleft)
+        tool_bg_surf.fill(BG_COLOUR)
+        tool_bg_rect = money_bg_surf.get_rect(
+            topleft=self.all_tool_rect.topleft
+        ).inflate(100, 100)
+        self.display_surface.blit(
+            tool_bg_surf,
+            tool_bg_rect,
+            self.all_tool_rect.inflate(300, 400),
+            special_flags=pygame.BLEND_RGBA_MULT,
+        )
+
+        self.display_surface.blit(inv_amount_surf, inv_amount_rect)
+        self.display_surface.blit(money_surf, money_rect)
         self.display_surface.blit(tool_surf, tool_rect)
+        self.display_surface.blit(coin_surf, coin_rect)
